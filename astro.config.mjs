f2aae8b86d392a565be32c93a4872646d9f0a455
Antonio Abril <antonio.abril@kraken.tech>
2025-12-24 15:12:34 +0100

    Fix path in vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
 // @ts-check
+import path from "path"
+import { fileURLToPath } from "url"
 import sitemap from "@astrojs/sitemap"
 import vercel from "@astrojs/vercel"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "./src"),
+			},
+		},
 		build: {
 			// Additional optimizations
 			cssCodeSplit: true,
