0289d3afdfdfcec6d43f738029c32f5856737f1c
Antonio Abril <antonio.abril@kraken.tech>
2026-04-05 19:55:32 +0200

    add: video as path in global project

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig({
 		resolve: {
 			alias: {
 				"@": path.resolve(__dirname, "./src"),
+				"@videos": path.resolve(__dirname, "./src/videos"),
 				"@styles": path.resolve(__dirname, "./src/styles"),
 			},
 		},
